d62c6d88e1a758d7fdbd3146834919df87ed11d7
Thomas Jalabert <thom.jalabert@gmail.com>
2018-09-08 14:07:42 +0200

    UI update change bootstrap to material design

--- autoSchedule/autoSchedule/forms.py
+++ autoSchedule/autoSchedule/forms.py
@@ -4,20 +4,25 @@ from easy_select2 import Select2Multiple, Select2Mixin, select2_modelform
 from django.forms import formset_factory
 
 class SessionForm(forms.Form):
-    session_range = forms.DateField(widget=forms.TextInput(attrs={ 'class':'form-control datepicker'}))
+    session_range = forms.DateField(widget=forms.TextInput(
+        attrs={ 'class':'form-control datepicker', 'placeholder':'Range'}))
 
 class WorkerForm(forms.Form):
-    name = forms.CharField(max_length=50, widget=forms.TextInput({"class": "form-control"}))
+    name = forms.CharField(max_length=50, widget=forms.TextInput({"class": "form-control", 'placeholder': 'Worker\'s name'}))
     # self.unavailable_periodes = unavailable_periodes
     # preferences
 
 class TaskForm(forms.Form):
-    task_type = forms.CharField(widget=forms.TextInput({"class": "form-control"}))
-    nb_workers = forms.IntegerField(widget=forms.TextInput({"class": "form-control input-sm"}))
-    start = forms.DateField(widget=forms.TextInput({"class": "form-control input-sm"}))
-    duration = forms.DurationField(widget=forms.TextInput({"class": "form-control input-sm"}))
+    task_type = forms.CharField(widget=
+        forms.TextInput({"class": "form-control", 'placeholder': 'Task\'s name'}))
+    nb_workers = forms.IntegerField(widget=
+        forms.TextInput({"class": "form-control input-sm", 'placeholder': 'People needed'}))
+    start = forms.DateField(widget=
+        forms.TextInput({"class": "form-control input-sm", 'placeholder': 'Start'}))
+    duration = forms.DurationField(widget=
+    forms.TextInput({"class": "form-control input-sm", 'placeholder': 'Duration'}))
     #difficulty = forms.IntegerField(widget=forms.TextInput({"class": "form-control input-md"}))
-    each_days = forms.BooleanField(widget=forms.TextInput({"class": "form-control input-sm"}))
+    each_days = forms.BooleanField()
 
 TaskFormSet = formset_factory(TaskForm)
 WorkerFormSet = formset_factory(WorkerForm)
